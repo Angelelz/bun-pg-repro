1036c946f2221e63b2b958c10265b5b907b510b2
Angelelz <angellopez20@gmail.com>
2023-10-17 23:33:52 -0400

    shorter

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ import postgres from 'postgres';
 const client = postgres("postgres://root:root@localhost:5432/postgres");
 
 async function main() {
-  try {
-    const res = await client`SELECT ${'hello world!'}::text as message`
-    console.log(res) // Hello world!
-  } catch (err) {
-    console.error(err);
-  } finally {
-    await client.end()
-  }
+  const res = await client`SELECT ${'hello world!'}::text as message`
+  console.log(res) // Hello world!
+  return client
 }
 
-main().then(() => console.log("done")).catch(console.error);
+main()
+  .then(() => console.log("done"))
+  .catch(console.error)
+  .finally(() => client.end());
 
